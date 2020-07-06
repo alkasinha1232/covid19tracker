ddfe7a5bf3d6902ca085e48cb401e0772b8b6086
alka <sinhaalka169@gmail.com>
2020-07-06 15:53:17 +0530

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 4200);
 
 app.get('/*', function(req, res){
     res.sendFile(path.join(_dirname +'dist/index.html'));
