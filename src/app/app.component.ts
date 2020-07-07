b21d2d5f4cab2720e5f6cce06990a70421a3e159
alka <sinhaalka169@gmail.com>
2020-07-07 10:11:54 +0530

    code commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { Covid19Service } from './covid19.service';
 import { Chart } from 'chart.js';  
-// import { Data } from '../../app/Data';
-// import { Servicepage } from '../servicepage.service';
 
 @Component({
   selector: 'app-root',
@@ -11,47 +9,49 @@ import { Chart } from 'chart.js';
 })
 export class AppComponent {
   states = ['Delhi', 'Bihar', 'Haryana', 'Gujarat'];
+  activestate = 'Delhi';
   // data: Data[];  
   userdetails:any;
-  xaxes = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg'];  
-  yaxes1 = [18, 34, 53, 56, 74, 80, 87];  
-  yaxes2 = [11, 44, 43, 63, 63, 66, 78]; 
-  yaxes3 = []; 
-  yaxes4 = [];
-  yaxes5 = [];
-  yaxes6 = []; 
-  yaxes7 = [];
-  yaxes8 = [];
-  yaxes9: any;
-  yaxes10: any;
-  Linechart: any;  
+  // xaxes = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg'];  
+  // yaxes1 = [18, 34, 53, 56, 74, 80, 87];  
+  // yaxes2 = [11, 44, 43, 63, 63, 66, 78]; 
+  totalConfirmed = []; 
+  totalConfirmedNew = [];
+  totalActive = [];
+  totalActiveNew = []; 
+  activedate = [];
+  activedateNew = [];
+  confirmedCasesIndian: any;
+  activecaseIndia: any;
+  covidGraph: any;  
   constructor( private servicepageService : Covid19Service) { }
 
   ngOnInit() {
     this.getcovideTracker(); 
   } 
+  
 getcovideTracker = () => {
   this.servicepageService.getcovideTracker().subscribe(resp => {
     this.userdetails = resp;
-    this.yaxes9 = this.userdetails.data[(this.userdetails.data).length-1].summary.confirmedCasesIndian;
-    this.yaxes10 = this.yaxes9 - this.userdetails.data[(this.userdetails.data).length-1].summary.discharged-this.userdetails.data[(this.userdetails.data).length-1].summary.deaths;
+    this.confirmedCasesIndian = this.userdetails.data[(this.userdetails.data).length-1].summary.confirmedCasesIndian;
+    this.activecaseIndia = this.confirmedCasesIndian - this.userdetails.data[(this.userdetails.data).length-1].summary.discharged-this.userdetails.data[(this.userdetails.data).length-1].summary.deaths;
     for(let i = 0; i<(this.userdetails.data).length; i++) {
       for(let j = 0; j<(this.userdetails.data[i].regional).length; j++){
-        if(this.userdetails.data[i].regional[j].loc == 'Delhi') {
-          this.yaxes3[i] = this.userdetails.data[i].regional[j].totalConfirmed;
-          this.yaxes7[i] = this.userdetails.data[i].day;
-          this.yaxes5[i] = this.yaxes3[i]-this.userdetails.data[i].regional[j].deaths-
+        if(this.userdetails.data[i].regional[j].loc == this.activestate) {
+          this.totalConfirmed[i] = this.userdetails.data[i].regional[j].totalConfirmed;
+          this.activedate[i] = this.userdetails.data[i].day;
+          this.totalActive[i] = this.totalConfirmed[i]-this.userdetails.data[i].regional[j].deaths-
           this.userdetails.data[i].regional[j].discharged;
         }
       }
     }
     for(let k = 0; k<11; k++) {
-      this.yaxes4[k] = this.yaxes3[this.yaxes3.length-k-1]; 
-      this.yaxes6[k] = this.yaxes5[this.yaxes5.length-k-1]; 
-      this.yaxes8[k] = this.yaxes7[this.yaxes7.length-k-1]
+      this.totalConfirmedNew[k] = this.totalConfirmed[this.totalConfirmed.length-k-1]; 
+      this.totalActiveNew[k] = this.totalActive[this.totalActive.length-k-1]; 
+      this.activedateNew[k] = this.activedate[this.activedate.length-k-1]
     } 
-    this.chartracker(this.yaxes4.reverse(), this.yaxes6.reverse(), this.yaxes8.reverse(), 
-    this.yaxes9, this.yaxes10);
+    this.chartracker(this.totalConfirmedNew.reverse(), this.totalActiveNew.reverse(), this.activedateNew.reverse(), 
+    this.confirmedCasesIndian, this.activecaseIndia);
   })
 }
 
@@ -99,7 +99,7 @@ getcovideTracker = () => {
 // }
 
 chartracker = (a, b, c, d, e) => {
-  this.Linechart = new Chart('canvas', {  
+  this.covidGraph = new Chart('canvas', {  
     type: 'line',  
     data: {  
       // labels: this.xaxes,  
@@ -155,12 +155,13 @@ chartracker = (a, b, c, d, e) => {
   }); 
 }
 
-onSelect(states) { 
-  this.states = null;
+onSelect(statename) { 
+  this.activestate = null;
   for (var i = 0; i < this.states.length; i++)
   {
-    if (this.states[i] == states) {
-      // this.states = this.states[i];
+    if (this.states[i] == statename) {
+      this.activestate = this.states[i];
+      this.getcovideTracker();
       // https://www.talkingdotnet.com/bind-select-dropdown-list-in-angular-js-2/
     }
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -10,6 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent
   ],
   imports: [
+    FormsModule, 
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
